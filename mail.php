4259cf38bd010527ca1cd397649a7670d05684a5
home-despot <42472792+home-despot@users.noreply.github.com>
2020-11-20 11:02:46 -0800

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='23.html' style='text-decoration:none;font-family:arial;color=#4285f4'>Take me to the Zoom link</a>";
+?>
