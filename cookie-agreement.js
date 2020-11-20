cde99d53930851ecaf6281bea115e3be1c72e991
home-despot <42472792+home-despot@users.noreply.github.com>
2020-11-20 10:51:15 -0800

    Delete cookie-agreement.js

--- cookie-agreement.js
+++
@@ -1,8 +0,0 @@
-if(localStorage.getItem('cookieSeen') != 'shown'){
-  $(".cookie-banner").delay(100).fadeIn();
-  localStorage.setItem('cookieSeen','shown')
-}
-
-$('.close').click(function(e) {
-$('.cookie-banner').fadeOut(); 
-});
